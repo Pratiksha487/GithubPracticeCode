9ccc9777c57e3b53203687d315400fc18b50294a
prate <prate@LAPTOP-7ADBBLHR>
2022-06-15 11:33:21 +0200

    By pratiksha eclipse

--- GitProject/src/com/GitDemo/Demo.java
+++ GitProject/src/com/GitDemo/Demo.java
@@ -12,6 +12,11 @@ public class Demo {
 		System.out.println("m2 from git");
 	}
 	
+	public void m3()
+	{
+		System.out.println("m3 from demo eclipse");
+	}
+	
 	public static void main(String[] args) 
 	{
 		Demo d=new Demo();
