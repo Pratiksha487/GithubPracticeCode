d00b446f387dd380277455a85cdfa6782ef3cbb0
prate <prate@LAPTOP-7ADBBLHR>
2022-06-15 11:10:42 +0200

    By Pratiksha 15-06-2022

---
+++ GitProject/src/com/GitDemo/Demo.java
@@ -0,0 +1,17 @@
+package com.GitDemo;
+
+public class Demo {
+
+	public void m1()
+	{
+		System.out.println("m1 from demo eclipse");
+	}
+	
+	public static void main(String[] args) 
+	{
+		Demo d=new Demo();
+		
+		d.m1();
+	}
+
+}
